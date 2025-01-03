f282d5b67c0cfdafe6d027cc132b78c741451c4c
Sabrina-Prity <sabrinaprity898@gmail.com>
2025-01-03 15:42:31 +0600

    Done

--- sweet_haven/settings.py
+++ sweet_haven/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = env("SECRET_KEY")
 DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
-# CSRF_TRUSTED_ORIGINS = [" "]
+CSRF_TRUSTED_ORIGINS = ["https://sweet-haven.onrender.com"]
 
 
 # Application definition
